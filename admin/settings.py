be081fad4521864cae1beb41f1b34a0690105e23
zorojurio <zorojurio@gmail.com>
2021-12-13 13:06:25 +0530

    static and media files configuration

--- admin/settings.py
+++ admin/settings.py
@@ -122,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+   os.path.join('static')
+]
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'cdn', 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'cdn', 'media')
